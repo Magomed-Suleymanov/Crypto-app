d3b9ffd531113643fa59a7c7149f6886f483e4a9
suleymanov <kinjudo@mail.ru>
2021-10-03 11:09:50 +0300

    форматирование кода

--- src/components/Main/Homepage.jsx
+++ src/components/Main/Homepage.jsx
@@ -3,7 +3,7 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
-const { Title } = Typography
+const { Title } = Typography;
 
 const Homepage = () => {
   return (
